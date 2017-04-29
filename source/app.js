dcc7e7e6431bc316261d29e6bb997b3bf0c75375
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-04-29 10:45:02 +0100

    this child parameter

--- source/app.js
+++ source/app.js
@@ -1,3 +1,7 @@
 /**
  * Created by ciaranroche on 28/04/2017.
  */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var reactElement = React.createElement('h1', { className: 'header' } , 'This is React');
+ReactDOM.render(reactElement, document.getElementById('react-application'));
