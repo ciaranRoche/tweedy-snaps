da518ec797c7592baa73d66f43c7df0b6b571c0c
ciaranRoche <ciaranroche@hotmail.co.uk>
2017-04-30 18:14:27 +0100

    Stream Tweet component added

---
+++ source/components/StramTweet.react.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Header = require('./Header.react');
+var Tweet = require('./Tweet.react');
+
+var StreamTweet = React.createClass({
+    render: function(){
+        console.log('[tweedy-snaps] StreamTweet: Running render()');
+
+        return(
+            <section>
+                <Header text={this.state.headerText}/>
+                <Tweet tweet={this.props.tweet} onImageClick={this.props.onAddTweetToCollection}/>
+            </section>
+        );
+    }
+});
+
+module.exports = StreamTweet;
